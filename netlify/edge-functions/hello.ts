fd3aa5013b4604360b22c0dac939b329af198063
moqataa-root <149231298+moqataa-root@users.noreply.github.com>
2023-12-22 16:07:33 +0400

    fix

--- netlify/edge-functions/hello.ts
+++ netlify/edge-functions/hello.ts
@@ -1,6 +1,12 @@
-export default async function handler(req: Request) {
-    const res = await fetch("https://buyisraelitech.com");
-    return new Response(res.body, res);
-}
 
+export default async (request: Request) => {
+
+    const res = await fetch("https://buyisraelitech.com", {
+        "headers": {
+            "Accept": "text/html,application/xhtml+xml"
+        }
+    });
+
+    return new Response(res.body, res);
+};
 export const config = { path: "/test" };
